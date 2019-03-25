c56400d36c4a85b877175b1b6e8919a6c204e539
C-viya <45985561+C-viya@users.noreply.github.com>
2019-03-25 10:33:43 +0800

    登录校验以及书籍分页

--- src/content/Newbooks.vue
+++ src/content/Newbooks.vue
@@ -24,7 +24,7 @@ export default {
   },
   created() {
     this.$ajax({
-      url: '/api/getHotBook',
+      url: '/api/getAllBook',
       method: 'post',
     }).then(res => {
       console.log(res)
