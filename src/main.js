872ee13e13bf482bd292588314af127f05e571fb
SalmonDaze <sa1mondz114@gmail.com>
2019-03-30 12:18:16 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -22,6 +22,7 @@ axios.interceptors.response.use((response) => {
 
 Vue.use(ElementUI);
 let socket = io.connect('http://192.168.2.73:3000')
+
 Vue.config.productionTip = false;
 Vue.prototype.$socket = socket
 Vue.prototype.$ajax = axios;
